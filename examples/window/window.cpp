b5f0b197afe1f503d428d870368d862211d46f19
Kass <kass.grzelak@proton.me>
2026-08-12 17:16:49 +0100

    Added explanatory comments to window example.

--- examples/window/window.cpp
+++ examples/window/window.cpp
@@ -6,18 +6,22 @@
 
 int main()
 {
+	// Initialize the gal library.
 	gal::init();
 
+	// Create the window, as well as an OpenGL context to go with it.
 	gal::Window window{800, 600, "Window Example"};
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
+	// Window loop.
 	while (!window.shouldClose())
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
-		window.swapBuffers();
-		window.pollEvents();
+		window.swapBuffers(); // Swap window buffers (i.e., update the screen).
+		window.pollEvents(); // Poll for window events (button presses, pressing the close button, etc.).
 	}
 
+	// Free all of gal's allocated resources.
 	gal::terminate();
 	return 0;
 }
